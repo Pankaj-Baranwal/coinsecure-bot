b8af95a62a39a8ce7764ebec1a8aaf7ba77196bd
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-10-31 17:27:06 +0530

    Added more APIs

--- coinsecure.py
+++ coinsecure.py
@@ -7,14 +7,23 @@ API_KEY = "ENTER_YOUR_API_KEY"
 
 def consumeGETRequests(_endpoint, _extras):
 	_params = {"accept": "application/json"}.items() + _extras.items()
-	r = requests.get(url = BASE_URL+_endpoint, params = _params)
+
+	try:
+		r = requests.get(url = BASE_URL+_endpoint, params = _params)
+	except requests.exceptions.ConnectionError:
+		print("Connection refused")
+		return -404
 	data = r.json()
 	return data
 
 def modifyUserData(_endpoint, _extras):
 	_params = {"accept": "application/json"}.items() + _extras.items()
 	print (_params)
-	r = requests.put(url = BASE_URL+_endpoint, data = _params, headers = {"Authorization" : API_KEY})
+	try:
+		r = requests.put(url = BASE_URL+_endpoint, data = _params, headers = {"Authorization" : API_KEY})
+	except requests.exceptions.ConnectionError:
+		print("Connection refused")
+		return -404
 	data = r.json()
 	return data
 
@@ -79,9 +88,29 @@ def placeNewSellOrder(_endpoint, _rate, _volume):
 	else:
 		return data
 
+'''
+[{u'vol': 9800000, u'rate': 38450000}, {u'vol': 40100000, u'rate': 38490000}, {u'vol': 570500000, u'rate': 38500000}]
+'''    
+def getBidOrders(_endpoint, _max):
+	data = consumeGETRequests(_endpoint, {'max': _max})
+	if (data['success']):
+		return data['message']
+	else:
+		return -1;
+
+'''
+[{u'vol': 1000000, u'rate': 38270001}, {u'vol': 1300000, u'rate': 38270000}, {u'vol': 13500000, u'rate': 38267200}]
+'''    
+def getAskOrders(_endpoint, _max):
+	data = consumeGETRequests(_endpoint, {'max': _max})
+	if (data['success']):
+		return data['message']
+	else:
+		return -1;
+
 def placeNewBuyOrder(_endpoint, _rate, _volume):
 	data = modifyUserData(_endpoint, {"rate" : _rate * UNIT_RUPEE, "vol" : int(_volume * UNIT_BTC)})
-	if data['success']:
+	if (data['success']):
 		return 1
 	else:
 		return data
@@ -90,6 +119,8 @@ def placeNewBuyOrder(_endpoint, _rate, _volume):
 # print(getHighestBidRate(_endpoint = "/exchange/bid/high"))
 # print(getMin24Hrs(_endpoint = "/exchange/min24Hr"))
 # print(getMax24Hrs(_endpoint = "/exchange/max24Hr"))
-# print (getPastTrades(_endpoint = "/exchange/trades", _max = 2))
+# print (getPastTrades(_endpoint = "/exchange/trades", _max = 10))
 # print(placeNewSellOrder(_endpoint = "/user/exchange/ask/new", _rate = 400000, _volume = 0.010))
 # print(placeNewSellOrder(_endpoint = "/user/exchange/bid/new", _rate = 300000, _volume = 0.010))
+# print (getAskOrders(_endpoint = "/exchange/ask/orders", _max = 3))
+# print (getBidOrders(_endpoint = "/exchange/bid/orders", _max = 3))
