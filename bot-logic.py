2590505aa045be8bb73326f8cb0a9fe3aece38cd
Utkarsh Mittal <utkarsh.mittal@icloud.com>
2017-11-08 20:18:40 +0530

    update init conditions

--- bot-logic.py
+++ bot-logic.py
@@ -23,8 +23,8 @@ from time import sleep
 
 # 1 = increasing slope. -1 = decreasing slope.
 # Slope calculated by (current - previous) rate. = 1 if increasing. = -1 if decreasing
-ask_orders_slope = 0
-bid_orders_slope = 0
+ask_orders_slope = int(raw_input("ask_orders_slope"))
+bid_orders_slope = int(raw_input("bid_orders_slope"))
 
 previous_ask_rate = 0
 previous_bid_rate = 0
@@ -37,9 +37,21 @@ while True:
 	if latest_ask_order == -1 or latest_bid_order == -1:
 		sleep(5)
 		continue
+	previous_ask_rate = latest_ask_order[0]['rate']
+	previous_bid_rate = latest_bid_order[0]['rate']
+	break
+
+while True:
+	latest_ask_order = cs.getAskOrders(_max = 1)
+	latest_bid_order = cs.getBidOrders(_max = 1)
+
+	# In case some query failed to get executed
+	if latest_ask_order == -1 or latest_bid_order == -1:
+		sleep(5)
+		continue
 
 	# If you want to buy, consider this
-	difference_in_rates = latest_ask_order[0]['rate'] - previous_ask_rate['rate']
+	difference_in_rates = latest_ask_order[0]['rate'] - previous_ask_rate
 	if ask_orders_slope == -1 and difference_in_rates > min_dist_from_minmax:
 		# CHECK IF WE HAVE MONEY, IF YES: PLACE BUY ORDER
 		inr_balance = cs.getUserINRBalance()
@@ -51,7 +63,7 @@ while True:
 		previous_ask_rate = latest_ask_order[0]['rate']
 
 	# If you want to sell, consider this
-	difference_in_rates = previous_bid_rate['rate'] - latest_bid_order[0]['rate']
+	difference_in_rates = previous_bid_rate - latest_bid_order[0]['rate']
 	if bid_orders_slope == 1 and difference_in_rates > min_dist_from_minmax:
 		# CHECK IF WE HAVE BTC, IF YES: PLACE SELL ORDER
 		btc_balance = cs.getUserBTCBalance()
