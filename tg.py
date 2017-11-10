1b8d5e6e335247da57bad2ff09fdf6462c2a54f7
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-11-11 03:27:51 +0530

    Changed secret key location

--- tg.py
+++ tg.py
@@ -1,9 +1,9 @@
-import BOT_TOKEN # contains BOT_TOKEN
+from API import BOT_TOKEN # contains BOT_TOKEN
 import telegram as tgram # pip install telegram-send
 
 CHAT_ID = -195455600 # chat id for the BTC telegram group
 
-bot = tgram.Bot(token=BOT_TOKEN.BOT_TOKEN)
+bot = tgram.Bot(token=BOT_TOKEN)
 
 def sendMessage(message):
 	bot.send_message(chat_id = CHAT_ID, text = message)
